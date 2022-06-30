2ebefe63b69120c53c31c794b4f3bb0a2d31ac83
Alexandre Roblain <alexrob260@gmail.com>
2022-06-30 20:40:45 +0200

    refacto script.js post mentorat

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,3 @@
-// Constante pour injection d'objets //
-
-const QUERY_ITEMS = document.getElementById("items");
-
 // Constante qui stocke l'url de l'API //
 
 const SERVER_URL = "http://localhost:3000/api/products/";
@@ -24,18 +20,18 @@ fetch(SERVER_URL)
 // Fonction construction de contenu (buildContent)
 
 function buildContent(item) {
-    for (let i = 0; i < item.length; i++) {
-      const PRODUCT_ITEM =   
-
-// Injection du code HTML 
-
-  `<a href="./product.html?id=${item[i]._id}">
+  // Constante pour injection d'objets //
+  const queryItems = document.getElementById("items");
+  for (let i = 0; i < item.length; i++) {
+    const productItem =
+      // Injection du code HTML
+      `<a href="./product.html?id=${item[i]._id}">
       <article>
           <img src="${item[i].imageUrl}" alt="${item[i].altTxt}">
           <h3 class="productName">${item[i].name}</h3>
-          <p class="productDescription">${item[i].description}</p>
+          <p class="productDescription">${item[i].description}</p>  
       </article>
   </a>`;
-      QUERY_ITEMS.insertAdjacentHTML("afterbegin", PRODUCT_ITEM);
-    }
+    queryItems.insertAdjacentHTML("beforeend", productItem);
   }
+}
