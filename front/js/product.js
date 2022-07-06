533ed7e6d6d96e0d2bbf98b4c37243c952c2fe1a
Alexandre Roblain <alexrob260@gmail.com>
2022-07-06 18:26:32 +0200

    Redaction product.js - (1) : affichage détails produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,35 +1,27 @@
-//////////////////////////////////////////// Page ////////////////////////////////////////////
+// Page produit //
 
-// Constante qui stocke l'url de l'API //
-
-const SERVER_URL = "http://localhost:3000/api/products/";
-
-// Retourne l'url de la page
-
-let string = window.location.href;
-
-let url = new URL(string);
+let str = window.location.href;
+let url = new URL(str);
 let idProduct = url.searchParams.get("id");
-console.log(string);
+console.log(idProduct);
+let article = "";
 
-// Création des variables de sélection ( couleurs & qté )
-
-let choiceColor = document.querySelector("#colors");
-let choiceQuantity = document.querySelector("#quantity");
+const COLOR_PICKED = document.querySelector("#colors");
+const QUANTITY_PICKED = document.querySelector("#quantity");
 
 getProduct();
 
-// Fonction de récupération des articles de l'API
+// Fonction de récupération des détails du produit (fetch) //
 
 function getProduct() {
-  fetch(SERVER_URL + idProduct)
+  fetch("http://localhost:3000/api/products/" + idProduct)
     .then((response) => {
       return response.json();
     })
 
-    // Répartition des données de l'API dans le DOM
     .then(async function (resultatAPI) {
       article = await resultatAPI;
+      console.table(article);
       if (article) {
         buildPage(article);
       }
@@ -38,35 +30,36 @@ function getProduct() {
       console.log(err);
     })
 }
-
-// Fonction de construction de la page
+// Fonction de construction de la page produit //
 
 function buildPage(article) {
-  // Photo
+
+  // Image //
   let productImg = document.createElement("img");
   document.querySelector(".item__img").appendChild(productImg);
+
+  // Image src & alt //
   productImg.src = article.imageUrl;
   productImg.alt = article.altTxt;
 
-  // Titre
+  // Titre //
   let productName = document.getElementById('title');
   productName.innerHTML = article.name;
 
-  // Prix
+  // Prix //
   let productPrice = document.getElementById('price');
   productPrice.innerHTML = article.price;
 
-  // Description
+  // Description //
   let productDescription = document.getElementById('description');
   productDescription.innerHTML = article.description;
 
-  // Choix des couleurs
+  // Choix des couleurs //
   for (let colors of article.colors) {
+    console.table(colors);
     let productColors = document.createElement("option");
     document.querySelector("#colors").appendChild(productColors);
     productColors.value = colors;
     productColors.innerHTML = colors;
   }
 }
-
-//////////////////////////////////////////// BOUTON CMD ////////////////////////////////////////////
